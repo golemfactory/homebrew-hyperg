c4da2cec690af0d5c7bf90d9e5b6120f640b139b
Marek Franciszkiewicz <marek@golem.network>
2018-11-29 13:10:41 +0100

    Version 0.2.9

--- hyperg.rb
+++ hyperg.rb
@@ -1,9 +1,9 @@
 class Hyperg < Formula
   desc 'Hyperdrive Client for Golem'
   homepage 'https://github.com/golemfactory/hyperg'
-  url 'https://github.com/mfranciszkiewicz/golem-hyperdrive/releases/download/v0.2.6/hyperg_0.2.6_darwin-x64.tar.gz'
-  version '0.2.6'
-  sha256 '935468276637a5c8a6770cd1c9fe64195f4cbe8d2597cce1ec004aed61a8a238'
+  url 'https://github.com/mfranciszkiewicz/golem-hyperdrive/releases/download/v0.2.9/hyperg_0.2.9_darwin-x64.tar.gz'
+  version '0.2.9'
+  sha256 '6e165d9f3f7119615af80e548dd3d22ff5cd8ce535d26016ce039ce55ffc3baf'
 
 
   def install
