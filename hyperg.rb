f5a4d28b20c9f738af1fb77c86d1f3e5c451984c
Marek Franciszkiewicz <marek@golem.network>
2018-12-06 22:42:44 +0100

    Ownership change

--- hyperg.rb
+++ hyperg.rb
@@ -1,7 +1,7 @@
 class Hyperg < Formula
   desc 'Hyperdrive Client for Golem'
-  homepage 'https://github.com/golemfactory/hyperg'
-  url 'https://github.com/mfranciszkiewicz/golem-hyperdrive/releases/download/v0.2.9/hyperg_0.2.9_darwin-x64.tar.gz'
+  homepage 'https://github.com/golemfactory/golem-hyperdrive'
+  url 'https://github.com/golemfactory/golem-hyperdrive/releases/download/v0.2.9/hyperg_0.2.9_darwin-x64.tar.gz'
   version '0.2.9'
   sha256 '6e165d9f3f7119615af80e548dd3d22ff5cd8ce535d26016ce039ce55ffc3baf'
 
