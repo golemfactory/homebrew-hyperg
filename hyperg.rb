9e442e97d66ac30598fca54b01ff359ce0f0fa65
Marek Franciszkiewicz <marek@golem.network>
2019-07-16 18:59:32 +0200

    Bump version to 0.3.3

--- hyperg.rb
+++ hyperg.rb
@@ -3,15 +3,9 @@ class Hyperg < Formula
   homepage 'https://github.com/golemfactory/simple-transfer'
 
   stable do
-    version '0.3.2'
-    url 'https://github.com/golemfactory/simple-transfer/releases/download/0.3.2/hyperg_0.3.2_darwin-x64.tar.gz'
-    sha256 'a554172e39117c608897145c20ce2ee0fca4bdbf9d41cf3dfa51150c41677a98'
-  end
-
-  devel do
-    version '0.3.2'
-    url 'https://github.com/golemfactory/simple-transfer/releases/download/0.3.2/hyperg_0.3.2_darwin-x64.tar.gz'
-    sha256 'a554172e39117c608897145c20ce2ee0fca4bdbf9d41cf3dfa51150c41677a98'
+    version '0.3.3'
+    url 'https://github.com/golemfactory/simple-transfer/releases/download/0.3.3/hyperg_0.3.3_darwin-x64.tar.gz'
+    sha256 'ead239ac77476deaa32d7c1dc4b1a43accf220090b5e0de47774053da9383f70'
   end
 
   def install
