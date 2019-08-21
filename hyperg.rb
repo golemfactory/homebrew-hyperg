bddf031fe5941316d25c75c9a5f09063fd029f1c
Marek Franciszkiewicz <marek@golem.network>
2019-08-21 13:48:52 +0200

    devel version 0.3.6

--- hyperg.rb
+++ hyperg.rb
@@ -8,6 +8,12 @@ class Hyperg < Formula
     sha256 'ead239ac77476deaa32d7c1dc4b1a43accf220090b5e0de47774053da9383f70'
   end
 
+  devel do
+    version '0.3.6'
+    url 'https://github.com/golemfactory/simple-transfer/releases/download/0.3.5/hyperg_0.3.6_darwin-x64.tar.gz'
+    sha256 '9ef9bc81498307a33a0d311e9a8e0d92eea609087cb9374eb4bc2668868929e1'
+  end
+
   def install
     bin.install 'hyperg'
   end
