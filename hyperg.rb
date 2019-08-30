e6fa563a44ecc72b9c248bec6caccb86f5f72140
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2019-08-30 16:41:04 +0200

    Shasum update for dev 0.3.6

--- hyperg.rb
+++ hyperg.rb
@@ -11,7 +11,7 @@ class Hyperg < Formula
   devel do
     version '0.3.6'
     url 'https://github.com/golemfactory/simple-transfer/releases/download/0.3.6/hyperg_0.3.6_darwin-x64.tar.gz'
-    sha256 '9ef9bc81498307a33a0d311e9a8e0d92eea609087cb9374eb4bc2668868929e1'
+    sha256 '5c2c7c59c771809e885b7037ad325e0ccc144043ccb894436ceefc436342b84b'
   end
 
   def install
